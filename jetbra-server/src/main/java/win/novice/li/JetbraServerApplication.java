7ff2206df92a31d78daa28880add41536ca740fc
novice.li <novice.li@outlook.com>
2023-11-25 16:30:09 +0800

    init

--- jetbra-server/src/main/java/win/novice/li/JetbraServerApplication.java
+++ jetbra-server/src/main/java/win/novice/li/JetbraServerApplication.java
@@ -3,13 +3,9 @@ package win.novice.li;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.cert.PKIXBuilderParameters;
-import java.util.HashSet;
-
 @SpringBootApplication
 public class JetbraServerApplication {
-    public static void main(String[] args) throws InvalidAlgorithmParameterException {
+    public static void main(String[] args) {
         SpringApplication.run(JetbraServerApplication.class,args);
     }
 }
