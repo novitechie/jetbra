a7858ac83f5348fa3f879de57bdb84a227eb65bd
novice.li <novice.li@outlook.com>
2024-04-07 22:29:05 +0800

    Compatible with 2024.1

--- jetbra-agent/src/main/java/com/novitechie/jetbra/LicensingFacadeAdvice.java
+++ jetbra-agent/src/main/java/com/novitechie/jetbra/LicensingFacadeAdvice.java
@@ -4,16 +4,18 @@ import net.bytebuddy.asm.Advice;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 public class LicensingFacadeAdvice {
 
     @Advice.OnMethodExit
     public static void intercept(@Advice.Return(readOnly = false) Date expirationDate) {
         RuntimeException e = new RuntimeException();
+        Pattern pattern = Pattern.compile("\\A\\p{ASCII}*\\z");
         for (StackTraceElement stackTraceElement : e.getStackTrace()) {
-            if (stackTraceElement.getFileName() == null){
+            if (!pattern.matcher(stackTraceElement.getMethodName()).matches()){
                 Calendar calendar = Calendar.getInstance();
-                calendar.add(Calendar.DAY_OF_MONTH, 50);
+                calendar.add(Calendar.DAY_OF_MONTH, 180);
                 expirationDate = calendar.getTime();
                 break;
             }
