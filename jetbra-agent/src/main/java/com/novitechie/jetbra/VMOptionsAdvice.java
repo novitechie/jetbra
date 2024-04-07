2c4c8fafa4d086e336af0112f8ce3a760316bb23
novice.li <novice.li@outlook.com>
2024-04-07 22:20:24 +0800

    compatible with 2024.1

--- jetbra-agent/src/main/java/com/novitechie/jetbra/VMOptionsAdvice.java
+++ jetbra-agent/src/main/java/com/novitechie/jetbra/VMOptionsAdvice.java
@@ -3,14 +3,16 @@ package com.novitechie.jetbra;
 import net.bytebuddy.asm.Advice;
 
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 public class VMOptionsAdvice {
 
     @Advice.OnMethodExit
     public static void intercept(@Advice.Return(readOnly = false) Path r) throws Exception {
         RuntimeException e = new RuntimeException();
+        Pattern pattern = Pattern.compile("\\A\\p{ASCII}*\\z");
         for (StackTraceElement stackTraceElement : e.getStackTrace()) {
-            if (stackTraceElement.getFileName() == null){
+            if (!pattern.matcher(stackTraceElement.getMethodName()).matches()){
                 r = null;
                 break;
             }
